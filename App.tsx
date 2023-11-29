12d7c0817c01805c77e650aa1186ae951d1f4595
Hayata Suenaga <hayata@expensify.com>
2023-11-29 15:49:44 -0800

    chore: reproduce bug

--- App.tsx
+++ App.tsx
@@ -3,8 +3,19 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-400">
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center">
+      <View className="h-10 bg-hexcolor">
+        <Text>Works</Text>
+      </View>
+      <View className="h-10 bg-hexcolor/20">
+        <Text>Works</Text>
+      </View>
+      <View className="h-10 bg-hslcolor">
+        <Text>Works</Text>
+      </View>
+      <View className="h-10 bg-hslcolor/20">
+        <Text>Don't work</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hexcolor: "#00ff00",
+        hslcolor: "hsl(120, 100%, 50%)",
+      },
+    },
   },
   plugins: [],
 };
