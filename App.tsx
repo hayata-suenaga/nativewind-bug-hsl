c2f153585c05c25c180aa810d20df38499ac408b
Hayata Suenaga <hayata@expensify.com>
2023-11-29 16:01:03 -0800

    chore: add explanations

--- App.tsx
+++ App.tsx
@@ -5,16 +5,16 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
       <View className="h-10 bg-hexcolor">
-        <Text>Works</Text>
+        <Text>Hex color without opacity</Text>
       </View>
       <View className="h-10 bg-hexcolor/20">
-        <Text>Works</Text>
+        <Text>Hex color with opacity</Text>
       </View>
       <View className="h-10 bg-hslcolor">
-        <Text>Works</Text>
+        <Text>Hsl color without opacity</Text>
       </View>
       <View className="h-10 bg-hslcolor/20">
-        <Text>Don't work</Text>
+        <Text>Hsl color with opacity</Text>
       </View>
       <StatusBar style="auto" />
     </View>
---
+++ README.md
@@ -0,0 +1,13 @@
+# NativeWind Bug
+
+### How to run
+
+`npm start`
+
+### Bug description
+
+When a customer color is defined using the Hsl format in `tailwind.config.js`, the color opacity syntax of Tailwind `bg-{color-name}/{opacity-value}` doesn't work.
+
+### What you should see
+
+<img src="./screenshot.jpeg" alt="screenshot" width="200"/>
